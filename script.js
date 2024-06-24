c6d2404f98356bfd8699c887d2974d652f431ff9
Katyayini-K <kkatyayini1@gmail.com>
2024-06-25 02:01:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let time = document.querySelector('.time');
+let heading = document.querySelector('.heading');
+// buttons
+let startBtn = document.querySelector('.startBtn');
+let pauseBtn = document.querySelector('.pauseBtn');
+let resumeBtn = document.querySelector('.resumeBtn');
+let resetBtn = document.querySelector('.resetBtn');
